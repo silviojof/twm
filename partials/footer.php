bdeacdc12b8ba6968130dbbe64012771255fceb8
Silvio Oliveira <silviojof@gmail.com>
2017-04-15 17:18:17 -0700

    holy saturday commit

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,29 @@
+		<footer class="row expanded main-footer">
+			<div class="row">
+					<div class="small-12 medium-4 column main-footer-address">
+						<h2>adress</h2>
+						<p>19834 Butternut Trail <br> Farmington, MN 55024</p>
+					</div>
+					<div class="small-12 medium-4 column main-footer-social">
+						<h2>social networks</h2>
+						<p>
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+							<i class="fa fa-facebook" aria-hidden="true"></i>
+							<i class="fa fa-instagram" aria-hidden="true"></i>
+						</p>
+					</div>
+					<div class="small-12 medium-4 column main-footer-cities">
+						<h2>towns we've worked</h2>
+						<p><strong>Wisconsin</strong>: Webster, Siren, Frederic, Grantsburg, Danbury, Spooner, Rice Lake, Chetek, Superior, River Falls, Madison, Milltown, Balsam Lake, Dresser, St. Croix Falls, Birchwood. <strong>Minnesota</strong>: Hinckley, Duluth, Hastings, Brooklyn Center, Maple Grove, Farmington, Minneapolis, Saint Paul, Arden Hills, Glenwood, Redwood Falls</p>
+					</div>
+			</div>
+		</footer>
+		<footer class="row expanded sub-footer">
+			<p>&copy;2003-2016 Trustworthy Music. All rights reserved.</p>
+		</footer>
+
+
+
 		<script
   		src="http://code.jquery.com/jquery-3.2.1.min.js"
   		integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
