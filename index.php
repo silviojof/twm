a9aba0fef09e80b8282f7bef2c61f1285af1591a
thisisvana <thisisvana@gmail.com>
2017-04-15 17:17:39 -0700

    structure

--- index.php
+++ index.php
@@ -1,6 +1,78 @@
 <?php include('partials/header.php'); ?>
 <?php include('partials/navbar.php'); ?>
 
+  <section>
+    <div class="hero-area">
+      <div class="row expanded">
+        <div class="banner">
+          <img src="img/banner.jpg" alt="dj music knobs">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="mission">
+      <div class="row">
+        <div class="about">
+          <h1>Our Mission</h1>
+          <p>Trustworthy Music provides professional DJ services to you at a reasonable price.
+            We are based in Siren, Wisconsin, and Farmington, Minnesota, allowing us to serve
+            the entire northwestern Wisconsin region along with the Twin Cities and surrounding areas.
+            Trustworthy Music is co-owned and operated by Joe Cremin and Steve Macke, and each have many
+            years of training and experience to provide you with the best service possible. We use high quality
+            equipment to ensure there are no technical difficulties during your event.
+            We have many years of experience in the DJ industry, and are glad to tailor our performance to your event.
+            Whether that means playing certain styles of music for your wedding or playing only edited and appropriate
+            songs for your school dance, we are happy to accommodate your needs! We pride ourselves in providing a great
+            night of entertainment for you, as well as not breaking your budget. We keep our prices very affordable,
+            but never sacrifice quality. Many companies charge over twice as much as we do, but we work hard to be the best value you will find.
+            With a HUGE selection of songs to choose from we have something for everyone.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="events">
+      <div class="row">
+        <div class="services">
+          <ul>
+            <li>icon - wedding</li>
+            <li>icon - school dances</li>
+            <li>icon - corporate events</li>
+            <li>icon - graduation</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="wedding-cta">
+    <div class="row">
+      <div class="wedding-para">
+        <h3>Because a wedding is one of the largest events to plan, we have a dedicated wedding page to help you.</h3>
+      </div>
+      <div class="wedding-btn">
+        <button type="button" name="button"><p>See Our wedding Page</p></button>
+      </div>
+    </div>
+  </div>
+  <section>
+    <div class="contact-form">
+      <div class="row">
+        <div class="c-form">
+          <form class="" action="#" method="post">
+            <input type="text" name="name" placeholder="Name">
+            <input type="email" name="email" placeholder="Email">
+            <input type="number" name="phone" placeholder="Phone Number">
+            <textarea name="message" placeholder="Message"></textarea>
+            <input type="submit" name="submit">
+          </form>
 
+        </div>
+
+      </div>
+
+    </div>
+  </section>
 
 <?php include('partials/footer.php'); ?>
